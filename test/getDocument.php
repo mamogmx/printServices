c45549bb4ce4680f98f436e59eff6568dcf7a5bc
mamogmx <marco.carbone@gmx.com>
2014-06-26 16:51:23 +0200

    ....

--- test/getDocument.php
+++ test/getDocument.php
@@ -7,25 +7,35 @@
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
         <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
-        
+        <style>
+            div.container{
+                width:500px;
+                padding:10px;
+            }
+            input.data{
+                width:200px;
+                margin-left:20px;
+                float:right;
+            }
+        </style>
     </head>
     <body>
         <form method="POST" action="../services/xGetDocument.php" target="_new">
-            <div>
+            <div class="container">
                 <label for="app">Applicazione</label>
-                <input type="text" name="app" id="app" value=""/>
+                <input class="data" type="text" name="app" id="app" value=""/>
             </div>
-            <div>
+            <div class="container">
                 <label for="project">Progetto</label>
-                <input type="text" name="project" id="project" value=""/>
+                <input class="data" type="text" name="project" id="project" value=""/>
             </div>
-            <div>
+            <div class="container">
                 <label for="id">Id</label>
-                <input type="text" name="id" id="id" value=""/>
+                <input class="data" type="text" name="id" id="id" value=""/>
             </div>
-            <div>
+            <div class="container">
                 <label for="filename">File</label>
-                <input type="text" name="filename" id="filename" value=""/>
+                <input class="data" type="text" name="filename" id="filename" value=""/>
             </div>
             <input type="submit" value="Invia"/>
         </form>
