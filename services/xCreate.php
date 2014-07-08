8fc645f47446d819090649164c99c4147208ef3c
mamogmx <marco.carbone@gmx.com>
2014-07-08 16:34:31 +0200

    some changes to xCreate

--- services/xCreate.php
+++ services/xCreate.php
@@ -16,145 +16,177 @@ require_once "../config.php";
 $debugName=DBG_DIR."debug-create.txt";
 
 
-	require_once LIB_DIR."tbs_class.php";
-	require_once LIB_DIR."tbs_plugin_opentbs.php";
-	$TBS = new clsTinyButStrong; // new instance of TBS
-	$TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN); // load OpenTBS plugin
-	
-	//ACQUISIZIONE DATI DI REQUEST
-	$filename = $_REQUEST["filename"];
-	$modello=$_REQUEST["model"];
-	$data=$_REQUEST["data"];
+require_once LIB_DIR."tbs_class.php";
+require_once LIB_DIR."tbs_plugin_opentbs.php";
+$TBS = new clsTinyButStrong; // new instance of TBS
+$TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN); // load OpenTBS plugin
 
-	$app=$_REQUEST["app"];
-	$mode=(isset($_REQUEST["mode"]))?($_REQUEST["mode"]):("");
-	$group=$_REQUEST["group"];
-	$project=$_REQUEST["project"];
-	$id=($_REQUEST["id"])?($_REQUEST["id"]):(rand(1,100000));
+//ACQUISIZIONE DATI DI REQUEST
+$filename = $_REQUEST["filename"];
+$modello=$_REQUEST["model"];
+$data=$_REQUEST["data"];
 
-	//RIMOZIONE slashes del POST
-	if ( in_array( strtolower( ini_get( 'magic_quotes_gpc' ) ), array( '1', 'on' ) )){
-		$_REQUEST = array_map( 'stripslashes', $_REQUEST);
+$app=$_REQUEST["app"];
+$mode=(isset($_REQUEST["mode"]))?($_REQUEST["mode"]):("");
+$group=$_REQUEST["group"];
+$project=$_REQUEST["project"];
+$id=($_REQUEST["id"])?($_REQUEST["id"]):(rand(1,100000));
+
+//RIMOZIONE slashes del POST
+if ( in_array( strtolower( ini_get( 'magic_quotes_gpc' ) ), array( '1', 'on' ) )){
+    $_REQUEST = array_map( 'stripslashes', $_REQUEST);
+}
+$request=$_REQUEST;
+//DECODIFICA DELLA STRINGA JSON CON DATI
+$_REQUEST['data']=json_decode($_REQUEST["data"],true);
+
+
+
+//DEBUG DEI DATI DI REQUEST
+debug($debugName,$_REQUEST,'a+'); 
+
+
+
+//MODELLO DI STAMPA
+if(filter_var($modello, FILTER_VALIDATE_URL)){
+    $f=fopen($modello,'rb');
+    $doc= stream_get_contents($f);
+    fclose($f);
+    $name=pathinfo($modello,PATHINFO_BASENAME);
+    $modelName="/tmp/$name";
+    $f=fopen($modelName,'w');
+    if (fwrite($f,$doc)) debug($debugName,"File $name scritto correttamente",'a+'); 
+    else{
+        $msg="Il modello $name non è stato scritto correttamente sul server";
+        debug($debugName,$msg,'a+');
+        $result=Array("success"=>0,"message"=>$msg);
+        header('Content-Type: application/json; charset=utf-8');
+        print json_encode($result);
+        return;
     }
-	$request=$_REQUEST;
-	//DECODIFICA DELLA STRINGA JSON CON DATI
-	$_REQUEST['data']=json_decode($_REQUEST["data"],true);
-	
-	
+    fclose($f);
+}
+else{
+    $modelDir=($project)?(MODEL.$project.DIRECTORY_SEPARATOR):(MODEL);
+    $modelName = ($group)?($modelDir.$app.DIRECTORY_SEPARATOR.$group.DIRECTORY_SEPARATOR.$modello):($modelDir.$app.DIRECTORY_SEPARATOR.$modello);
+}
 
-	//DEBUG DEI DATI DI REQUEST
-	debug($debugName,$_REQUEST,'a+'); 
-	
+if(!file_exists($modelName)){
+    $msg="Im modello $modelName non è stato trovato!";
+    debug($debugName,$msg,'a+');
+    $result=Array("success"=>0,"message"=>$msg);
+    header('Content-Type: application/json; charset=utf-8');
+    print json_encode($result);
+    return;
+}
 
-	
-	//MODELLO DI STAMPA
-	if(filter_var($modello, FILTER_VALIDATE_URL)){
-		$f=fopen($modello,'rb');
-		$doc= stream_get_contents($f);
-		fclose($f);
-		$name=pathinfo($modello,PATHINFO_BASENAME);
-		$modelName="/tmp/$name";
-		$f=fopen($modelName,'w');
-		if (fwrite($f,$doc)) debug($debugName,"File $name scritto correttamente",'a+'); 
-		else{
-			debug($debugName,"File $name non è scritto correttamente",'a+');
-		}
-		fclose($f);
-	}
-	else{
-		$modelDir=($project)?(MODEL.$project.DIRECTORY_SEPARATOR):(MODEL);
-		$modelName = ($group)?($modelDir.$app.DIRECTORY_SEPARATOR.$group.DIRECTORY_SEPARATOR.$modello):($modelDir.$app.DIRECTORY_SEPARATOR.$modello);
-	}
-	
-	if(!file_exists($modelName)){
-		debug($debugName,"Template $modelName not found!",'a+');
-		print json_encode(Array("success"=>-1,""=>"$modelName"));
-		return; 
-	}
+debug($debugName,"Mode : $mode\nLoading Template $modelName",'a+');
 
-	if(!file_exists($modelName)){
-		debug($debugName,"Template $modelName not found!",'a+');
-		print json_encode(Array("success"=>-1,""=>"$modelName"));
-		return; 
-	}
-	debug($debugName,"Mode : $mode\nLoading Template $modelName",'a+');
-		
-	$TBS->LoadTemplate($modelName);
-	$TBS->SetOption('noerr',true);
-	debug($debugName,"Template Loaded",'a+');
-	$data=$_REQUEST["data"];
-	
-	switch($app){
-		case "ordinanze":
-			$keys=array_keys($data);
-			$excludedItems=Array('numero_registro_cronologico','data_pubblicazione');
-			foreach($excludedItems as $k ){
-				if (in_array($k,$keys) && !$data[$k]){
-					unset($data[$k]);
-				}
-			}
-			debug($debugName,$data,'a+');
-			break;
-		default:
-			$dbgName="get.debug";
-	}
-	
-	if($data) {
-		$data["oggi"]=date('d/m/Y');
-	   $TBS->LoadTemplate($modelName);
-	   $TBS->SetOption('noerr',true);
-		array_walk_recursive($data, 'decode');
-		mergeFields($TBS,$data);
+$TBS->LoadTemplate($modelName);
+$TBS->SetOption('noerr',true);
+debug($debugName,"Template Loaded",'a+');
+$data=$_REQUEST["data"];
 
-	if ($TBS->Plugin(OPENTBS_FILEEXISTS, 'word/header1.xml')){
-			$TBS->LoadTemplate('#word/header1.xml');
-			mergeFields($TBS,$data);
-		}
-		if ($TBS->Plugin(OPENTBS_FILEEXISTS, 'word/footer1.xml')){
-				$TBS->LoadTemplate('#word/footer1.xml');
-				mergeFields($TBS,$data);
-		}    
-	}	
-	$docDir=($project)?(DOC_DIR."$project".DIRECTORY_SEPARATOR):(DOC_DIR);
-	if ($mode=="show"){
-		$docFile = $docDir."$app/$id/$filename";
-		if (!file_exists($docDir."$app")) mkdir($docDir."$app");
-		if (!file_exists($docDir."$app/$id")) mkdir($docDir."$app/$id");
-		$TBS->Show(OPENTBS_FILE,$docFile);		 
-		//$TBS->Show(OPENTBS_STRING);
-		//$out=$TBS->Source;
-		$encode=mb_detect_encoding($out);
+switch($app){
+    case "ordinanze":
+        $keys=array_keys($data);
+        $excludedItems=Array('numero_registro_cronologico','data_pubblicazione');
+        foreach($excludedItems as $k ){
+                if (in_array($k,$keys) && !$data[$k]){
+                        unset($data[$k]);
+                }
+        }
+        debug($debugName,$data,'a+');
+        break;
+    default:
+        $dbgName="get.debug";
+}
 
-		$ff=fopen($docFile,'r');
-		$doc=fread($ff,filesize($docFile));
-
-		//$out=(mb_detect_encoding($item)=='UNICODE')?(utf8_encode($item)):($item);
-		print $doc;
-		return;
-		//print json_encode(Array("success"=>1,'filename'=>'nofile',"output"=>$out));
-		
-		//return;
-	}
-	else{
-		
-		if (!file_exists($docDir."$app")) mkdir($docDir."$app");
-		if (!file_exists($docDir."$app/$id")) {
-			if (!mkdir($docDir."$app/$id")){
-				debug($debugName,"Impossibile creare la directory ".$docDir."$app/$id",'a+');
-			}
-		}
+if($data) {
+   $data["oggi"]=date('d/m/Y');
+   $TBS->LoadTemplate($modelName);
+   $TBS->SetOption('noerr',true);
+   array_walk_recursive($data, 'decode');
+   mergeFields($TBS,$data);
+   $HeaderAndFooter = $TBS->PlugIn(OPENTBS_GET_HEADERS_FOOTERS);
+   for($i=0;$i<count($HeaderAndFooter);$i++){
+       $f=$HeaderAndFooter[$i];
+       $TBS->LoadTemplate($f);
+       mergeFields($TBS,$data);
+   }
+}	
+$docDir=($project)?(DOC_DIR."$project".DIRECTORY_SEPARATOR):(DOC_DIR);
+switch($mode){
+    case "show":
         $docFile = $docDir."$app/$id/$filename";
+        if (!file_exists($docDir."$app")) mkdir($docDir."$app");
+        if (!file_exists($docDir."$app/$id")) mkdir($docDir."$app/$id");
         $TBS->Show(OPENTBS_FILE,$docFile);		 
-		if($TBS){
-			echo  json_encode(Array("success"=>1,'filename'=>$docFile));
-			debug($debugName,"Document $docFile created",'a+');
+        $ff=fopen($docFile,'r');
+        $doc=fread($ff,filesize($docFile));
+        fclose($ff);
+        header('Content-Type: application/application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+        print $doc;
+        return;
+        break;
+    default :
+        $docFile = $docDir."$app/$id/$filename";
+        if (!file_exists($docDir."$app")) mkdir($docDir."$app");
+        if (!file_exists($docDir."$app/$id")) {
+            if (!mkdir($docDir."$app/$id")){
+                $msg="Impossibile creare la directory ".$docDir."$app/$id";
+                $result=Array("success"=>0,"message"=>$msg);
+                debug($debugName,$msg,'a+');
+                header('Content-Type: application/json; charset=utf-8');
+                print json_encode($result);
+                return;
+            }
+        }
+        $TBS->Show(OPENTBS_FILE,$docFile);
+        if($TBS){
+            if($mode=="download"){
+                $result=Array("success"=>1,'filename'=>$docFile);
+            }
+            else{
+                $f=fopen($docfile,'r');
+                $text=fread($f,filesize($f));
+                fclose($f);
+                $result=Array("success"=>1,"file"=>  base64_encode($text));
+            }
+            $msg="Il file $filename è stato creato correttamente";
+            debug($debugName,$msg,'a+');
+            header('Content-Type: application/json; charset=utf-8');
+            print json_encode($result);
+            return;
+        }
+        else{
+            $msg="Sono stati riscontrati degli errori nella generazione del documento $filename";
+            debug($debugName,$msg,'a+');
+            $result=Array("success"=>0,"message"=>$msg);
+            header('Content-Type: application/json; charset=utf-8');
+            print json_encode($result);
+            return;
+        }
+        break;
+}
+if ($mode=="show"){
+        
+}
+else{
 
-		}
-		else{
-			echo json_encode(Array("success"=>-1,'filename'=>$docFile));
-			debug($debugName,"Document $docFile created",'a+');
-		}
-		return;
-	}
+    
+    
+    		 
+        if($TBS){
+                echo  json_encode();
+                debug($debugName,"Document $docFile created",'a+');
+
+        }
+        else{
+                echo json_encode(Array("success"=>-1,'filename'=>$docFile));
+                debug($debugName,"Document $docFile created",'a+');
+        }
+        return;
+}
 	
 ?>
